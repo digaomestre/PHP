e5823c763d5a45baa1cf3a1566a8ebdc4da11789
Rodrigo T. Souza <rodrigo_souza2013@yahoo.com.br>
2021-07-03 11:25:36 -0300

    Testar os tipos de números

--- fundamental/unidade07/numeros2.php
+++ fundamental/unidade07/numeros2.php
@@ -1,6 +1,7 @@
 <?php
     $salario = 1095;
     $gasolina = 4.55;
+    $telefone = "2526-5428";
 ?>
 
 <!doctype html>
@@ -16,11 +17,22 @@
         <?php
             // testar se é numérica
 
+             echo "O $salario é um número: ". is_numeric($salario). "</br>";   
+             echo "O $gasolina é um número: ". is_numeric($gasolina). "</br>";   
+             echo "O $telefone é um número: ". is_numeric($telefone). "</br>";   
 
             // testar se é inteiro
-
+            echo "---------------------------------------------------------</br>";
+            echo "O $salario é um inteiro: ". is_int($salario). "</br>";   
+            echo "O $gasolina é um inteiro: ". is_int($gasolina). "</br>";   
+            echo "O $telefone é um inteiro ". is_int($telefone). "</br>";
 
             // testar se é float
+
+            echo "---------------------------------------------------------</br>";
+            echo "O $salario é um float: ". is_float($salario). "</br>";   
+            echo "O $gasolina é um float: ". is_float($gasolina). "</br>";   
+            echo "O $telefone é um float ". is_float($telefone). "</br>";
         ?>
         
         
